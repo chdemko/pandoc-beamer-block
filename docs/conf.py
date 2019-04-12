fa7d962827d59ad792a78de4c4358a64706af6ef
Christophe Demko <chdemko@gmail.com>
2019-04-12 21:17:59 +0200

    Prepare next version

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,9 @@ copyright = "2019, Christophe Demko"
 author = "Christophe Demko"
 
 # The short X.Y version
-version = "0.1"
+version = "0.2"
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.2.0.dev0"
 
 # -- General configuration ---------------------------------------------------
 
