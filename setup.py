5c7a4ffb74e715cfbb3905d8ff1de4c1e3332de6
Christophe Demko <chdemko@gmail.com>
2019-04-12 21:19:50 +0200

    Use only python 3.6

--- setup.py
+++ setup.py
@@ -61,10 +61,7 @@ setup(
         "Topic :: Text Processing :: Filters",
         # Specify the Python versions you support HERE. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         # Natural language used
         "Natural Language :: English",
     ],
