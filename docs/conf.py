dba23c952343b0319037c9658709e3e24e4d2ff8
Christophe Demko <chdemko@gmail.com>
2024-01-21 20:51:29 +0100

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import importlib.metadata
 import os
 import sys
+
+from datetime import date
 
 sys.path.insert(0, os.path.abspath("../.."))
 
@@ -25,15 +28,13 @@ on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 # -- Project information -----------------------------------------------------
 
-project = "pandoc-beamer-block"
-copyright = "2018-2023, Christophe Demko"
-author = "Christophe Demko"
+project, release = os.popen("poetry version").readline().strip().split(" ")
+author = importlib.metadata.metadata(project)["Author"]
+year = date.today().year
+copyright = f"2018-{year}, {author}"
 
 # The short X.Y version
-version = "1.0"
-# The full version, including alpha/beta/rc tags
-release = "1.0.0.0"
-
+version = ".".join(release.split(".")[:2])
 
 # -- General configuration ---------------------------------------------------
 
